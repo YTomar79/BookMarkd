4bd227fa75ffd68f8f4b58ebb1027c8f8f0f2cc0
YTomar79 <166078547+YTomar79@users.noreply.github.com>
2024-07-12 13:01:28 -0400

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+    net_score = db.Column(db.Integer, default=0)
+
+class Vote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    review_id = db.Column(db.Integer, db.ForeignKey('review.id'), nullable=False)
+    vote_type = db.Column(db.Boolean, nullable=False)
 
